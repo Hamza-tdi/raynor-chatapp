55ad9bc4fd3a4051e261fe92d86fba9c785d00aa
hamza <hamza.el.bahlaoui@outlook.com>
2022-06-17 23:22:20 +0100

    initial commit

--- static/scripts/manage_rooms.js
+++ static/scripts/manage_rooms.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (const btn of btns) {
             btn.addEventListener('click', function() {
                 socket.emit('disable_room', {'room': btn.getAttribute('data-set')})
-                if (btn.innerText == 'enable'){
+                if (btn.innerText == 'Enable'){
                     btn.innerText = 'Disable'
                     btn.setAttribute('data-set', btn.getAttribute('data-set').replace('is_active', 'is_inactive'))
                 }
